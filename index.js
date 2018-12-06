3bcf8cef0306ae846f077c4ac6ed661b9dd2588a
Daniel Thompson <daniel.thompson@agencyq.com>
2018-12-06 17:45:12 -0500

    Trying an API call

--- index.js
+++ index.js
@@ -30,13 +30,37 @@ for (let [k,v] of Object.entries(params)) {
 
 authURL = authURL.slice(0,-1);
 
+let fetchOptions = {};
+
 if (location.hash) {
 	let params = {};
 	location.hash.slice(1).split('&').forEach(param => {
 		let [k,v] = param.split('=').map(v => decodeURIComponent(v));
 		params[k] = v;
 	});
-	console.log(params);
+
+	fetchOptions.headers = { Authorization: `Bearer ${params.access_token}` } };
+} else if (location.search && location.search.startsWith('?error')) {
+	let h3 = document.createElement('h3');
+	h3.textContent = "Trouble Logging In";
+	h3.style.color = 'red';
+	h3.style.fontSize = '3em';
+	document.body.appendChild(h3);
 } else {
 	window.location = authURL;
+}
+
+function getRecentPlays() {
+	let url = `https://api.spotify.com/v1/me/player/recently-played`;
+
+	fetch(url, fetchOptions).then(r => r.json()).then(data => {
+		console.log(window.__DATA__ = data);
+		let div = document.createElement('div');
+		div.textContent = JSON.stringify(data, true);
+		document.body.appendChild(div);
+	});
+}
+
+if (fetchOptions.headers) {
+	getRecentPlays();
 }
